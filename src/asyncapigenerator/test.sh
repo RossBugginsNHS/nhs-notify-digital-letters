f9c53d516b6577ea5c753a7d8e001c68b57ef56b
Ross Buggins <ross.buggins@nhs.net>
2025-11-18 16:08:47 +0000

    trying python3 -m pytest.

---
+++ src/asyncapigenerator/test.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+set -euo pipefail
+PYTHON=$1
+
+cd ../.. && \
+echo "Current working directory: $(pwd)" && \
+ls -la $HOME/.asdf/shims && \
+asdf reshim python && \
+whereis pytest && \
+whereis python3 && \
+echo "PATH IS $PATH" && \
+$PYTHON -m pytest src/asyncapigenerator/tests/ --cov=src/asyncapigenerator --cov-config=src/asyncapigenerator/pytest.ini --cov-report=html:src/asyncapigenerator/htmlcov --cov-report=term-missing --cov-report=xml:src/asyncapigenerator/coverage.xml
