2d25d0b9abc277941c7c4f06c918d792fad51dd9
Ross Buggins <ross.buggins@nhs.net>
2025-11-18 16:42:36 +0000

    pytest output path for which

--- src/asyncapigenerator/test.sh
+++ src/asyncapigenerator/test.sh
@@ -8,5 +8,7 @@ ls -la $HOME/.asdf/shims && \
 asdf reshim python && \
 whereis pytest && \
 whereis python3 && \
+which pytest && \
+which python3 && \
 echo "PATH IS $PATH" && \
 $PYTHON -m pytest src/asyncapigenerator/tests/ --cov=src/asyncapigenerator --cov-config=src/asyncapigenerator/pytest.ini --cov-report=html:src/asyncapigenerator/htmlcov --cov-report=term-missing --cov-report=xml:src/asyncapigenerator/coverage.xml
