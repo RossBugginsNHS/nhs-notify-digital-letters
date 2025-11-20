fbfcfb31651ed151b9140f75036e7c957e767c4f
Ross Buggins <ross.buggins@nhs.net>
2025-11-20 10:29:14 +0000

    Tests for other python.

--- src/test.sh
+++ src/test.sh
@@ -4,8 +4,14 @@ set -euo pipefail
 cd ./asyncapigenerator && \
 ./test.sh python3 && \
 cd .. && \
+
 cd ./eventcatalogasyncapiimporter && \
 ./test.sh python3 && \
 cd .. && \
+
+cd ./cloudeventjekylldocs && \
+./test.sh python3 && \
+cd .. && \
+
 cd ./cloudevents && \
 npm run test:unit
